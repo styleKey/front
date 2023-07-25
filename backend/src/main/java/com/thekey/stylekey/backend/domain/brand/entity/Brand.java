0eda0eb5d4c76414eca74c6b748f507daee43e70
jeondui <jeondui@gmail.com>
2023-07-26 02:26:40 +0900

    entity 설계

---
+++ backend/src/main/java/com/thekey/stylekey/backend/domain/brand/entity/Brand.java
@@ -0,0 +1,54 @@
+package com.thekey.stylekey.backend.domain.brand.entity;
+
+import com.thekey.stylekey.backend.domain.item.entity.Item;
+import com.thekey.stylekey.backend.domain.stylepoint.entity.StylePoint;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "brand")
+public class Brand {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "brand_id", nullable = false)
+    private Long id;
+
+    @Column(name = "brand_title", nullable = false)
+    private String title;
+
+    @Column(name = "brand_description", nullable = false)
+    private String description;
+
+    @Column(name = "brand_site_url", nullable = false)
+    private String site_url;
+
+    @Column(name = "brand_image", nullable = false)
+    private String image;
+
+    @OneToMany(mappedBy = "brand", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Item> items = new ArrayList<>();
+
+    // Brand : StylePoint (N : 1)
+    // 다대일 양방향 연관관계
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "stylepoint_id")
+    private StylePoint stylepoint;
+
+    @Builder
+    public Brand(String title, String description, String site_url, String image, StylePoint stylePoint) {
+        this.title = title;
+        this.description = description;
+        this.site_url = site_url;
+        this.image = image;
+        this.stylepoint = stylePoint;
+    }
+}
