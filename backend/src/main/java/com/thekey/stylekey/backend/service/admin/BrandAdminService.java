19bbaf77267ef214d870f5519a5ca54a1298e783
jeondui <jeondui@gmail.com>
2023-07-27 23:21:22 +0900

    관리자페이지 브랜드 서비스로직 구현

---
+++ backend/src/main/java/com/thekey/stylekey/backend/service/admin/BrandAdminService.java
@@ -0,0 +1,26 @@
+package com.thekey.stylekey.backend.service.admin;
+
+import com.thekey.stylekey.backend.model.brand.entity.Brand;
+import com.thekey.stylekey.backend.service.admin.dto.CreateBrandRequestDto;
+import com.thekey.stylekey.backend.service.admin.dto.UpdateBrandRequestDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface BrandAdminService {
+
+    // create
+    Brand createBrand(CreateBrandRequestDto brand);
+
+    // read only one
+    Brand findById(Long id);
+
+    // read all
+    Page<Brand> findAll(Pageable pageable);
+
+    // update
+    Brand updateBrand(Long id, UpdateBrandRequestDto brand);
+
+    // delete
+    void deleteBrand(Long id);
+
+}
