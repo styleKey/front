2ce349443b20ddbf9df6878bcd28be463684e688
yuni <hys990730@gmail.com>
2023-08-13 20:03:44 +0900

    feat 관리자: 메인 라우트 설정
    메인 라우트 설정으로 폼 별 링크 설정함.

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import StylePointForm from './components/StylePointForm';
+import CoordiLookForm from './components/CoordiLookForm';
+import BrandForm from './components/BrandForm';
+import CategoryForm from './components/CategoryForm';
+import ItemForm from './components/ItemForm';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/category">Category [0번대]</Link>
+            </li>
+            <li>
+              <Link to="/stylepoint">StylePoint [100번대]</Link>
+            </li>
+            <li>
+              <Link to="/brand">Brand [200번대]</Link>
+            </li>
+            <li>
+              <Link to="/item">Item [300번대]</Link>
+            </li>
+            <li>
+              <Link to="/coordilook">CoordiLook [400번대]</Link>
+            </li>
+          </ul>
+        </nav>
+
+        
+        <Routes>
+          <Route path="/stylepoint" element={<StylePointForm />} />
+          <Route path="/coordilook" element={<CoordiLookForm />} />
+          <Route path="/brand" element={<BrandForm />} />
+          <Route path="/category" element={<CategoryForm />} />
+          <Route path="/item" element={<ItemForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
