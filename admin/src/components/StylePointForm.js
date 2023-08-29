c64b3c4b0d7a1a78ee2c6aa7c5acc44ed7249b91
yuni <hys990730@gmail.com>
2023-08-29 20:49:35 +0900

    feat 관리자: styleForm 에서 브랜드 조회
    브랜드 조회시 null 만 출력

--- admin/src/components/StylePointForm.js
+++ admin/src/components/StylePointForm.js
@@ -6,6 +6,7 @@ function StylePointForm() {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [image, setImage] = useState('');
+  const [brands, setBrands] = useState([]);
   const [coordiLooks, setCoordiLooks] = useState([]);
   const [error, setError] = useState(null);
 
@@ -27,28 +28,6 @@ function StylePointForm() {
       handleError('StylePoint 가져오기 오류', error.message);
     }
   };
-  
-  const fetchCoordiLooks = async (coordiLookIds) => {
-    try {
-      const response = await fetch(`/admin/coordilooks?ids=${coordiLookIds.join(',')}`);
-      if (response.ok) {
-        const data = await response.json();
-        // Check if data is an array before setting it to the state
-        if (Array.isArray(data)) {
-          setCoordiLooks(data);
-          setError(null);
-        } else {
-          setCoordiLooks([]); // Set an empty array as a fallback
-          setError('코디룩 정보 가져오기 오류: 데이터 형식이 잘못되었습니다.');
-        }
-      } else {
-        handleError('코디룩 정보 가져오기 실패', response.status);
-      }
-    } catch (error) {
-      handleError('코디룩 정보 가져오기 오류', error.message);
-    }
-  };
-  
 
   const handleError = (message, errorDetail) => {
     const errorMessage = `${message}: ${errorDetail}`;
@@ -57,43 +36,27 @@ function StylePointForm() {
   };
 
   const handleStylePointClick = async (stylePoint) => {
-    setSelectedStylePoint(stylePoint);
-    setTitle(stylePoint.title);
-    setDescription(stylePoint.description);
-    setImage(stylePoint.image);
-    
-    // Check if coordiLookList is defined and is an array before calling map
-    if (stylePoint.coordiLookList && Array.isArray(stylePoint.coordiLookList)) {
-      await fetchCoordiLooks(stylePoint.coordiLookList.map(coordiLook => coordiLook.id));
-    } else {
-      // Handle the case where coordiLookList is undefined or not an array
-      setCoordiLooks([]);
-      setError('코디룩 정보 가져오기 오류: 데이터 형식이 잘못되었습니다.');
-    }
-  };
-  
-  const handleCreateStylePoint = async () => {
     try {
-      const newStylePointData = {
-        title,
-        description,
-        image // 이미지 URL도 포함하여 보냅니다.
-      };
-      const response = await fetch(`/admin/stylepoint/create`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newStylePointData),
-      });
+      const response = await fetch(`/admin/stylepoint/${stylePoint.id}`);
       if (response.ok) {
-        fetchStylePoints();
+        const data = await response.json();
+        setSelectedStylePoint(data.stylePoint);
+        setTitle(data.stylePoint.title);
+        setDescription(data.stylePoint.description);
+        setImage(data.stylePoint.image);
+
+        const fetchedBrands = data.brands;
+        const fetchedCoordiLooks = data.stylePoint.coordiLookList;
+
+        setBrands(fetchedBrands);
+        setCoordiLooks(fetchedCoordiLooks);
+
         setError(null);
       } else {
-        handleError('StylePoint 등록 실패', response.status);
+        handleError('StylePoint 조회 실패', response.status);
       }
     } catch (error) {
-      handleError('StylePoint 등록 오류', error.message);
+      handleError('StylePoint 조회 오류', error.message);
     }
   };
 
@@ -143,7 +106,7 @@ function StylePointForm() {
       </div>
       <div className="container">
         <div className="card">
-          <h3>{selectedStylePoint ? 'Update Style Point' : 'Create Style Point'}</h3>
+          <h3>Update Style Point</h3>
           <div className="input-container">
             <label>Title</label>
             <br />
@@ -159,11 +122,7 @@ function StylePointForm() {
             <br />
             <input type="text" value={image} onChange={(e) => setImage(e.target.value)} />
           </div>
-          {selectedStylePoint ? (
-            <button className="button" onClick={handleUpdateStylePoint}>Update Style Point</button>
-          ) : (
-            <button className="button" onClick={handleCreateStylePoint}>Create Style Point</button>
-          )}
+          <button className="button" onClick={handleUpdateStylePoint}>Update Style Point</button>
         </div>
       </div>
       <div className="container">
@@ -176,11 +135,15 @@ function StylePointForm() {
               <p><strong>Image:</strong> <img src={selectedStylePoint.image} alt="Style Point" /></p>
 
               <h4>Coordi Looks:</h4>
-              <ul>
-                {coordiLooks.map((coordiLook) => (
-                  <li key={coordiLook.id}>{coordiLook.name || 'NULL'}</li>
-                ))}
-              </ul>
+              {coordiLooks && coordiLooks.map((coordiLook) => (
+                <li key={coordiLook.id}>{coordiLook.name || 'NULL'}</li>
+              ))}
+
+ <h4>Brands:</h4>
+              {brands && brands.map((brand) => (
+                <li key={brand.id}>{brand.name || 'NULL'}</li>
+              ))}
+
             </div>
           )}
         </div>
