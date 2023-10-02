bec473b126e4c187b496a1614e94474f9353e335
jeondui <jeondui@gmail.com>
2023-10-02 22:36:15 +0900

    수정

--- backend/src/main/java/com/thekey/stylekey/backend/model/stylepoint/repository/StylePointRepository.java
+++ backend/src/main/java/com/thekey/stylekey/backend/model/stylepoint/repository/StylePointRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StylePointRepository extends JpaRepository<StylePoint, Long> {
-    Long findStylePointByTitle(String title);
 }
