cb2d23efab8827dd99284db8146e1c1b55b32823
jeondui <jeondui@gmail.com>
2023-10-02 22:36:09 +0900

    사용자페이지 테스트 컨트롤러 구현

--- backend/src/main/java/com/thekey/stylekey/backend/controller/user/UserTestController.java
+++ backend/src/main/java/com/thekey/stylekey/backend/controller/user/UserTestController.java
@@ -7,11 +7,9 @@ import com.thekey.stylekey.backend.service.user.dto.CreateTestResultRequestDto;
 import com.thekey.stylekey.backend.service.user.dto.CreateTestResultResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.weaver.ast.Test;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @RestController
@@ -19,32 +17,26 @@ import java.util.List;
 @RequestMapping("/user")
 @Slf4j
 public class UserTestController {
-    private final TestResultRepository testResultRepository;
-
     private final TestUserService testUserService;
 
-    @PostMapping("/test/save")
+    @PostMapping("/testResult/save")
     public ResponseEntity<CreateTestResultResponseDto> saveTestResult(@RequestBody CreateTestResultRequestDto requestDto) {
         TestResult savedResult = testUserService.saveTestResult(requestDto);
 
-        // CreateTestResultResponseDto를 생성하고 필요한 데이터를 설정
         CreateTestResultResponseDto responseDto = CreateTestResultResponseDto.builder()
                 .highest_stylepoint(savedResult.getHighest_stylepoint())
                 .lowest_stylepoint(savedResult.getLowest_stylepoint())
                 .build();
-
-        log.info("최고 점수:" + String.valueOf(savedResult.getHighest_stylepoint()));
-        log.info("최저 점수:" + String.valueOf(savedResult.getLowest_stylepoint()));
         return ResponseEntity.ok(responseDto);
     }
 
-    @GetMapping("test/result/{id}")
+    @GetMapping("testResult/{id}")
     public ResponseEntity<TestResult> getTestResultById(@PathVariable Long id) {
         TestResult testResult = testUserService.findById(id);
         return ResponseEntity.ok(testResult);
     }
 
-    @GetMapping("test/results")
+    @GetMapping("testResults")
     public ResponseEntity<List<TestResult>> getAllTestResult() {
         return ResponseEntity.ok(testUserService.findAll());
     }
