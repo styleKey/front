002b7024582f1d6e1ae9c1c12c8cc76f4c2455cd
jinsupark4255 <jin04007@naver.com>
2024-01-05 12:11:16 +0900

    Design: header UI 완성

--- frontend/src/components/layouts/Logobar.js
+++ frontend/src/components/layouts/Logobar.js
@@ -6,11 +6,13 @@ import InstaImage from "../../logo/Instagram-logo.png";
 function Logobar() {
   return (
     <div className="header">
-      <div className="logo_text">StyleKEY</div>
-      <div className="header_logos">
-        <img src={YotubeImage} className="header_logo1" alt="youtube" />
-        <img src={InstaImage} className="header_logo" alt="instagram" />
-        <div className="header_login">Login</div>
+      <div className="header_top">
+        <div className="logo_text">StyleKEY</div>
+        <div className="header_logos">
+          <img src={YotubeImage} className="header_logo1" alt="youtube" />
+          <img src={InstaImage} className="header_logo" alt="instagram" />
+          <div className="header_login">Login</div>
+        </div>
       </div>
     </div>
   );
